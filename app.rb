9ec3bcaae9d7904fd2e67bb7037cf4bf9feabc3b
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-18 14:15:36 -0400

    Added Views and Public folders, and moved files

--- app.rb
+++ app.rb
@@ -1,2 +1,31 @@
 require 'sinatra'
 require 'sendgrid-ruby'
+
+get "/" do
+    @page_title = "Friendly Bakery"
+end
+
+get "/bread" do
+    @page_title = "Friendly Bakery: Bread"
+end
+
+get "/cookies" do
+    @page_title = "Friendly Bakery: Cookies"
+end
+
+get "/cakes" do
+    @page_title = "Friendly Bakery: Cakes"
+end
+
+get "/muffins" do
+    @page_title = "Friendly Bakery: Muffins"
+end
+
+get "/about" do
+    @page_title = "About Friendly Bakery"
+end
+
+get "/contact" do
+    @page_title = "Contact Friendly Bakery"
+end
+
