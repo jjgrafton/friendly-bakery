b0f6b51a4e44c23b7d379ffe64d93e7b26841b66
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-18 16:49:32 -0400

    Added subheader to all template pages

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sendgrid-ruby'
 
 get "/" do
     @page_title = "Friendly Bakery"
+    erb :index
 end
 
 get "/bread" do
@@ -27,5 +28,24 @@ end
 
 get "/contact" do
     @page_title = "Contact Friendly Bakery"
+end
+
+post "" do
+    @email = params[:email]
+  @recipes = params[:recipes]
+
+  from = Email.new(email: jamiegrafton@gmail.com)
+  to = Email.new(email: @email)
+  subject = 'Friendly Bakery REcipes'
+  content = Content.new(type: 'text/plain', value: @recipes)
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['NEW_SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  if response.status_code == 401
+    error_hash = JSON.parse(response.body)
+    @errors = error_hash["errors"]
+  end 
+end 
 end
 
