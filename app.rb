f8d1c1f99708e2026c1f2a7e6cc4c52f73d4ced7
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-19 21:34:04 -0400

    README.md

--- app.rb
+++ app.rb
@@ -53,21 +53,23 @@ get "/contact" do
 end
 
 
-# post "" do
-#     @email = params[:email]
-#   @recipes = params[:recipes]
-
-#   from = Email.new(email: jamiegrafton@gmail.com)
-#   to = Email.new(email: @email)
-#   subject = 'Friendly Bakery REcipes'
-#   content = Content.new(type: 'text/plain', value: @recipes)
-#   mail = Mail.new(from, subject, to, content)
-
-#   sg = SendGrid::API.new(api_key: ENV['NEW_SENDGRID_API_KEY'])
-#   response = sg.client.mail._('send').post(request_body: mail.to_json)
-#   if response.status_code == 401
-#     error_hash = JSON.parse(response.body)
-#     @errors = error_hash["errors"]
-#   end 
-# end 
-# end  
+post "/email" do
+  @email = params[:email]
+  @recipes = (params :recipes)
+  from = Email.new(email: jamiegrafton@gmail.com)
+  to = Email.new(email: @email)
+  subject = 'Friendly Bakery REcipes'
+  content = Content.new(type: 'text/html', value: @recipes)
+  mail = Mail.new(from, subject, to, content)
+  sg = SendGrid::API.new(api_key: ENV['NEW_SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  if response.status_code == 401
+    error_hash = JSON.parse(response.body)
+    @errors = error_hash["errors"]
+  end 
+  puts response.status_code
+  puts response.body
+  puts response.parsed_body
+  puts response.headers
+end 
+end  
